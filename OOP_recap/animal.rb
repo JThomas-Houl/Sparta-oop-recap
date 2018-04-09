437e2d0eb5abfc3a49d2080bf37ce38c4edf0b60
Tech-A25 <tech-a25@Tech-A25s-MacBook-Air.local>
2018-04-09 15:18:27 +0100

    class over viewa dn recap

--- OOP_recap/animal.rb
+++ OOP_recap/animal.rb
@@ -3,5 +3,33 @@ require_relative './living_organism'
 
 class Animal < LivingOrganism
 
+    def self.traits
+        puts "Animals can breathe, eat, drink, speak and pro-create"
+    end
+
+    def breathe
+        puts "i can't breathe"
+    end
+
+    def eat
+        puts "nom nom nom"
+    end
+
+    def drink
+        puts "anybody thirsty"
+    end
+
+    def speak
+        puts "speak thy fair maden"
+    end
+
+    def proCreate
+        puts "get down with it"
+    end
+
 end
 
+
+# Animal.traits
+# mouse = Animal.new
+# mouse.speak
--- OOP_recap/animal_types/mammals.rb
+++ OOP_recap/animal_types/mammals.rb
@@ -1,6 +1,42 @@
 # This is the Mammals module that should be included for all mammals
 module Mammals
+    
+    class CommonTraits
+       
+
+        def feed_young
+            puts " all animals feed ther young with milk"
+        end
+
+    end
+
+
+    class Biped < CommonTraits
+        def legs
+            puts "i have 2 legs"
+        end
+    end
+
+    class Quadraped < CommonTraits
+        def self.legs
+            puts "i have 4 legs"
+        end
+    end
+
+    # def self.common_mamal_traits
+    #     puts 'Mammals are warm blooded and have vertebrae'
+    # end
 
 
 
 end
+
+# puts Mammals.common_mamal_traits
+
+# dolphine = Mammals::CommonTraits.new
+
+# dolphine.feed_young
+
+# jeff = Mammals::Biped.new
+# jeff.legs
+# Mammals::Quadraped.legs
